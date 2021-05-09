0b65a0cb2bbe462dc18402f239fd51271a1c79a3
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-09 22:45:19 +0200

    Add files via upload

--- src/backend/views/home_subview.blade.php
+++ src/backend/views/home_subview.blade.php
@@ -1,4 +1,4 @@
-<div class="home_module home_module micro12 small6 medium2">
+<div class="home_module micro12 small6 medium3">
     <h1><span class="fas fa-images"></span> Galerie obrazków</h1>
     <button id="btn_zwgall">Lista</button>
     <button id="btn_zwgaln">Nowa</button>
