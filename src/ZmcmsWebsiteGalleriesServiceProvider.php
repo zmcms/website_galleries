475947d1fc8dd3f8295918b1d4a369a068d3a6ad
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-14 12:41:21 +0200

    Update ZmcmsWebsiteGalleriesServiceProvider.php

--- src/ZmcmsWebsiteGalleriesServiceProvider.php
+++ src/ZmcmsWebsiteGalleriesServiceProvider.php
@@ -12,12 +12,12 @@ class ZmcmsWebsiteGalleriesServiceProvider extends ServiceProvider{
 	public function boot(){
 		$this->loadMigrationsFrom(__DIR__.'/migrations');
 		$this->publishes([
-			__DIR__.'/config' => base_path('config/'.Config('frontend.theme_name').'/zmcms/website_galleries'),
-			__DIR__.'/backend/css' => base_path('public/themes/'.Config('frontend.theme_name').'/backend/css/'),
-			__DIR__.'/backend/js' => base_path('public/themes/'.Config('frontend.theme_name').'/backend/js/'),
+			__DIR__.'/config' => base_path('config/zmcms/website_galleries'),
+			__DIR__.'/backend/css' => base_path('public/themes/zmcms/backend/css/'),
+			__DIR__.'/backend/js' => base_path('public/themes/zmcms/backend/js/'),
 			__DIR__.'/backend/views' => base_path('resources/views/themes/zmcms/backend'),
-			__DIR__.'/frontend/css' => base_path('public/themes/'.Config('frontend.theme_name').'/frontend/css/'),
-			__DIR__.'/frontend/js' => base_path('public/themes/'.Config('frontend.theme_name').'/frontend/js/'),
+			__DIR__.'/frontend/css' => base_path('public/themes/zmcms/frontend/css/'),
+			__DIR__.'/frontend/js' => base_path('public/themes/zmcms/frontend/js/'),
 			__DIR__.'/frontend/views' => base_path('resources/views/themes/zmcms/frontend'),
 		]);
 	}
